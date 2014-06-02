a575594ab6771c22e48f91c77b65c8fa67e190d8
Rishi Shah <rishihahs@gmail.com>
2014-06-02 18:42:07 -0500

    QR Data Types

---
+++ qrcode.go
@@ -0,0 +1,25 @@
+package qrmatrix
+
+/*
+#cgo pkg-config: libqrencode
+#include <stdlib.h>
+#include <qrencode.h>
+*/
+import "C"
+
+import "unsafe"
+
+// QRcode struct represents a QR Code
+type QRcode struct {
+	version, width int
+	data           []byte
+}
+
+// CreateQRCode creates a Go QR Code from a C QR Code
+func CreateQRCode(cqrcode *C.QRcode) *QRcode {
+	version := int(cqrcode.version)
+	width := int(cqrcode.width)
+	data := C.GoBytes(unsafe.Pointer(cqrcode.data), C.int(width*width))
+
+	return &QRcode{version: version, width: width, data: data}
+}
---
+++ qrcodelist.go
@@ -0,0 +1,27 @@
+package qrmatrix
+
+/*
+#cgo pkg-config: libqrencode
+#include <stdlib.h>
+#include <qrencode.h>
+*/
+import "C"
+
+import "unsafe"
+
+var qrcodeList *C.QRcode_List
+
+// CreateQRCodes creates multiple QR Codes using structured append
+func CreateQRCodes(data []byte) {
+	qrcodeList = C.QRcode_encodeDataStructured(C.int(len(data)), (*C.uchar)(unsafe.Pointer(&data[0])), C.int(40), C.QR_ECLEVEL_L)
+}
+
+// Next increments QRcode_List pointer to the next element
+func Next() {
+	qrcodeList = (*C.QRcode_List)(qrcodeList.next)
+}
+
+// Code returns a QRcode struct of the QR Code
+func Code() *QRcode {
+	return CreateQRCode(qrcodeList.code)
+}
