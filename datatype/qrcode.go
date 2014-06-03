5d13ccd89213dec2ac14fe10348a3283a3659ec9
Rishi Shah <rishihahs@gmail.com>
2014-06-03 00:47:01 -0500

    Make QRCode fields public

--- datatype/qrcode.go
+++ datatype/qrcode.go
@@ -11,15 +11,19 @@ import "unsafe"
 
 // QRcode struct represents a QR Code
 type QRcode struct {
-	version, width int
-	data           []byte
+	Version, Width int
+	Data           []byte
 }
 
 // CreateQRCode creates a Go QR Code from a C QR Code
 func CreateQRCode(cqrcode *C.QRcode) *QRcode {
+	if cqrcode == nil {
+		return nil
+	}
+
 	version := int(cqrcode.version)
 	width := int(cqrcode.width)
 	data := C.GoBytes(unsafe.Pointer(cqrcode.data), C.int(width*width))
 
-	return &QRcode{version: version, width: width, data: data}
+	return &QRcode{Version: version, Width: width, Data: data}
 }
