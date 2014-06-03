12db8a6e5ed8d8abf033412b5ced5054104db714
Rishi Shah <rishihahs@gmail.com>
2014-06-03 01:34:16 -0500

    Make QRImage flexible

--- image/qrimage.go
+++ image/qrimage.go
@@ -10,27 +10,19 @@ import (
 	"github.com/rishihahs/qrmatrix/datatype"
 )
 
-const (
-	// ModuleSize is the size of QR Code module
-	ModuleSize = 1
-
-	// Margin is the number of margin modules
-	Margin = 4
-
-	// CodesPerRow is the the number of QR Codes per row
-	CodesPerRow = 2
-)
+// Margin is the number of margin modules
+const Margin = 4
 
 var white = color.RGBA{R: uint8(255), G: uint8(255), B: uint8(255), A: uint8(255)}
 var black = color.RGBA{R: uint8(0), G: uint8(0), B: uint8(0), A: uint8(255)}
 
 // CreateImage creates the QR Code Matrix image
-func CreateImage() {
+func CreateImage(moduleSize, codesPerRow int, location string) {
 	// Create a new image
 	size := datatype.Size()
-	singlewidth := datatype.Code().Width*ModuleSize + Margin*2*ModuleSize
-	totalwidth := singlewidth * CodesPerRow
-	totalheight := singlewidth * (size/CodesPerRow + size%2)
+	singlewidth := datatype.Code().Width*moduleSize + Margin*2*moduleSize
+	totalwidth := singlewidth * codesPerRow
+	totalheight := singlewidth * (size/codesPerRow + size%2)
 	img := image.NewRGBA(image.Rect(0, 0, totalwidth, totalheight))
 
 	// Fill the image with white
@@ -43,7 +35,7 @@ func CreateImage() {
 
 	for count := 0; datatype.Code() != nil; count++ {
 
-		for n := 0; n < CodesPerRow && datatype.Code() != nil; n++ {
+		for n := 0; n < codesPerRow && datatype.Code() != nil; n++ {
 			data := datatype.Code().Data
 
 			for i := 0; i < len(data); i++ {
@@ -55,9 +47,9 @@ func CreateImage() {
 					colour = black
 				}
 
-				for y := 0; y < ModuleSize; y++ {
-					for x := 0; x < ModuleSize; x++ {
-						img.SetRGBA(singlewidth*n+Margin*ModuleSize+(i%width)*ModuleSize+x, singlewidth*count+Margin*ModuleSize+(i/width)*ModuleSize+y, colour)
+				for y := 0; y < moduleSize; y++ {
+					for x := 0; x < moduleSize; x++ {
+						img.SetRGBA(singlewidth*n+Margin*moduleSize+(i%width)*moduleSize+x, singlewidth*count+Margin*moduleSize+(i/width)*moduleSize+y, colour)
 					}
 				}
 			}
@@ -72,7 +64,7 @@ func CreateImage() {
 
 	datatype.Free()
 
-	fo, err := os.Create("/Users/shah/Desktop/image.png")
+	fo, err := os.Create(location)
 	if err != nil {
 		panic(err)
 	}
