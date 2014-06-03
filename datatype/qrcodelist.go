5dc7c60345654ba932b77fb183c6b8ee5db6fac0
Rishi Shah <rishihahs@gmail.com>
2014-06-03 14:15:09 -0500

    Split data and recursively add to list if data is too large

--- datatype/qrcodelist.go
+++ datatype/qrcodelist.go
@@ -7,7 +7,10 @@ package datatype
 */
 import "C"
 
-import "unsafe"
+import (
+	"syscall"
+	"unsafe"
+)
 
 var origList *C.QRcode_List
 var qrcodeList *C.QRcode_List
@@ -15,8 +18,36 @@ var qrcode *QRcode
 
 // CreateQRCodes creates multiple QR Codes using structured append
 func CreateQRCodes(data []byte) {
-	qrcodeList = C.QRcode_encodeDataStructured(C.int(len(data)), (*C.uchar)(unsafe.Pointer(&data[0])), C.int(40), C.QR_ECLEVEL_L)
-	origList = qrcodeList
+	a, err := C.QRcode_encodeDataStructured(C.int(len(data)), (*C.uchar)(unsafe.Pointer(&data[0])), C.int(40), C.QR_ECLEVEL_L)
+
+	// If data too large
+	if err == syscall.ERANGE {
+		split(data)
+	} else {
+		qrcodeList = a
+		origList = qrcodeList
+	}
+}
+
+// Split list and recursively build onto a new one
+func split(data []byte) {
+	size := len(data) / 2
+	var mainList *C.QRcode_List
+	CreateQRCodes(data[:size])
+	mainList = qrcodeList
+	tempList := mainList
+
+	CreateQRCodes(data[size-1 : len(data)])
+	for tempList.next != nil {
+		tempList = (*C.QRcode_List)(tempList.next)
+	}
+	for qrcodeList != nil {
+		tempList.next = qrcodeList.next
+		tempList = (*C.QRcode_List)(tempList.next)
+		qrcodeList = (*C.QRcode_List)(qrcodeList.next)
+	}
+	qrcodeList = mainList
+	origList = mainList
 }
 
 // Next increments QRcode_List pointer to the next element
